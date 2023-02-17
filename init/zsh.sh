3f6cf51f54ddbcc13f6d7922a78d5458a1b3fe21
Masaki Otsubo <kanagu.remix@gmail.com>
2023-02-17 23:00:50 +0900

    fix zsh

--- init/zsh.sh
+++ init/zsh.sh
@@ -10,8 +10,9 @@ if [ `which zsh` ]; then
 else
     echo "Installing zsh..."
     brew install zsh
-    sudo chsh -s $(which zsh)
+    # set default zsh
+    command -v zsh | sudo tee -a /etc/shells
+    sudo chsh -s "$(command -v zsh)" "${USER}"
+    # install oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
-
-
