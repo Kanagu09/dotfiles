692e334faab675ae58b0eed195a4c1deedcce0c5
Masaki Otsubo <kanagu.remix@gmail.com>
2023-02-17 21:36:36 +0900

    fix zsh.sh

--- init/zsh.sh
+++ init/zsh.sh
@@ -8,9 +8,9 @@ echo_error () {
 if [ `which zsh` ]; then
     echo "zsh is already installed"
 else
-    echo "Installing zshw..."
+    echo "Installing zsh..."
     brew install zsh
-    chsh -s $(which zsh)
+    sudo chsh -s $(which zsh)
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
