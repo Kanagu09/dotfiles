18c88edde2544b8fd6cba30f0b64b7729e878e17
Masaki Otsubo <kanagu.remix@gmail.com>
2023-02-20 21:51:54 +0900

    add link.sh

--- init/link.sh
+++ init/link.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env zsh
+
+PWD=$(pwd)
+
+FILES=".zshrc .zsh_alias"
+set -- $FILES
+
+for FILE in $FILES
+do
+    if [ ! -e ./etc/$FILE ]; then
+        cp $HOME/$FILE $PWD/etc/$FILE
+        rm $HOME/$FILE
+        ln -s $PWD/etc/$FILE $HOME/$FILE
+    fi
+done
