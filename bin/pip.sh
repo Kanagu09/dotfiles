053f089aca073dbd77e4509e14de44dd6c26f542
Masaki Otsubo <kanagu.remix@gmail.com>
2023-02-27 17:40:59 +0900

    add pip install

--- bin/pip.sh
+++ bin/pip.sh
@@ -2,10 +2,17 @@
 . ./lib/echo.sh
 
 # install
-APP="numpy"
-if `pip show $APP > /dev/null` ; then
-    echo_installed $APP
-else
-    echo_installing $APP
-    pip install numpy
-fi
+APPS="numpy matplotlib"
+set -- $APPS
+
+for APP in $APPS
+do
+    if `pip show $APP > /dev/null` ; then
+        echo_installed $APP
+    else
+        echo_installing $APP
+        pip install $APP
+    fi
+done
+
+
