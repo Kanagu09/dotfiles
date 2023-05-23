0ba1ffd4f2218d6ed1aa4c2e570bf176ca9f51fc
Masaki Otsubo <kanagu.remix@gmail.com>
2023-05-23 14:35:27 +0900

    edit script

--- init/homebrew.sh
+++ init/homebrew.sh
@@ -8,8 +8,7 @@ else
     echo "Installing homebrew..."
     mkdir $HOME/.linuxbrew
     curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.linuxbrew
-    echo 'eval "$($HOME/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.profile"
-    eval "$(/$HOME/.linuxbrew/bin/brew shellenv)"
+    echo "$($HOME/.linuxbrew/bin/brew shellenv)" >> "$HOME/.profile"
     sudo apt-get install build-essential
     source $HOME/.profile
     brew update
--- init/link.sh
+++ init/link.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env zsh
 
-PWD=$(pwd)
-
-FILES=".zshrc .zsh_alias"
-set -- $FILES
-
-for FILE in $FILES
-do
-    if [ -e $PWD/etc/$FILE ]; then
-        echo "make link of $FILE"
-        rm $HOME/$FILE
-        ln -s $PWD/etc/$FILE $HOME/$FILE
-    else
-        echo "copy and make link of $FILE"
-        cp $HOME/$FILE $PWD/etc/$FILE
-        rm $HOME/$FILE
-        ln -s $PWD/etc/$FILE $HOME/$FILE
-    fi
+# make link
+for FILE in $(find ./etc/ -type f); do
+    FILE=$(echo $FILE | sed "s/.\/etc\///")
+    echo "make link of $FILE"
+    ln -sf ./etc/$FILE $HOME/$FILE
 done
