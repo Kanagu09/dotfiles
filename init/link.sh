f81b6d9cb4d404d905b9798a8391878ad81838af
Masaki Otsubo <kanagu.remix@gmail.com>
2023-05-23 14:43:42 +0900

    fix link.sh

--- init/link.sh
+++ init/link.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
+PWD=$(pwd)
+
 # make link
 for FILE in $(find ./etc/ -type f); do
     FILE=$(echo $FILE | sed "s/.\/etc\///")
     echo "make link of $FILE"
-    ln -sf ./etc/$FILE $HOME/$FILE
+    ln -sf $PWD/etc/$FILE $HOME/$FILE
 done
